4d443c3057b02a876501f84b109902df5b6a7338
Isibor Eromosele Jason <56369075+isiboreromoselejason@users.noreply.github.com>
2024-02-27 23:58:11 -0500

    Added Summary block

--- Ássingment 2/Ássingment 2/Controllers/ReversedDirectionsController.cs
+++ Ássingment 2/Ássingment 2/Controllers/ReversedDirectionsController.cs
@@ -9,6 +9,17 @@ namespace Ássingment_2.Controllers
 {
     public class ReversedDirectionsController : ApiController
     {
+    
+        /// <summary>
+        /// Retrieves a reversed navigation instruction based on the specified direction and place.
+        /// </summary>
+        /// <param name="direction">The original direction, either 'R' for right or 'L' for left.</param>
+        /// <param name="place">The destination or location to turn into.</param>
+        /// <returns>
+        /// Returns a string message instructing to turn into the specified place in the reversed direction.
+        /// If the provided direction is invalid, an error message is returned.
+        /// </returns>
+
         [HttpGet]
         [Route("api/J3/ReverseDirections/{direction}/{place}")]
         public string ReverseDirections(string direction, string place)
